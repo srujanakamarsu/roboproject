171abd3295a90ece913fb1b25113b754f9ea12be
srujanakamarsu <srujanamythree@gmail.com>
2023-07-24 23:02:02 +0530

    append previous content

--- catalogue.sh
+++ catalogue.sh
@@ -1,42 +1,42 @@
 echo -e  "\e[36m>>>>>> create catalogue service file <<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>/tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> create mongodb repo file <<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> install node js repos <<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> install node js <<<<<<\e[0m"
-yum install nodejs -y &>/tmp/roboshop.log
+yum install nodejs -y &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> create application user <<<<<<\e[0m"
-useradd roboshop &>/tmp/roboshop.log
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> delete application directory <<<<<<\e[0m"
-rm -rf /app &>/tmp/roboshop.log
+rm -rf /app &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> create application directory <<<<<<\e[0m"
-mkdir /app &>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> download application content <<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> extract application content <<<<<<\e[0m"
 cd /app 
-unzip /tmp/catalogue.zip &>/tmp/roboshop.log
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 cd /app 
 
 echo -e  "\e[36m>>>>>> download nodejs dependencies <<<<<<\e[0m"
-npm install &>/tmp/roboshop.log
+npm install &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> install mongo client <<<<<<\e[0m"
-yum install mongodb-org-shell -y &>/tmp/roboshop.log
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> load catalogue schema <<<<<<\e[0m"
-mongo --host mongodb.vyshu.online </app/schema/catalogue.js &>/tmp/roboshop.log
+mongo --host mongodb.vyshu.online </app/schema/catalogue.js &>>/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>> start catalogue service <<<<<<\e[0m"
-systemctl daemon-reload &>/tmp/roboshop.log
-systemctl enable catalogue &>/tmp/roboshop.log
-systemctl restart catalogue &>/tmp/roboshop.log
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl restart catalogue &>>/tmp/roboshop.log
