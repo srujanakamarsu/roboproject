0f55ea4bdb645c7e54068f9af4c0258e7af8d78e
srujanakamarsu <srujanamythree@gmail.com>
2023-07-24 22:33:57 +0530

    colored comments

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,28 @@
-echo ">>>>>> create catalogue service file <<<<<<"
+echo -e  "\e36m>>>>>> create catalogue service file <<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
-echo ">>>>>> create mongodb repo file <<<<<<"
+echo -e  "\e36m>>>>>> create mongodb repo file <<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo ">>>>>> install node js repos <<<<<<"
+echo -e  "\e36m>>>>>> install node js repos <<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo ">>>>>> install node js <<<<<<"
+echo -e  "\e36m>>>>>> install node js <<<<<<\e[0m"
 yum install nodejs -y
-echo ">>>>>> create application user <<<<<<"
+echo -e  "\e36m>>>>>> create application user <<<<<<\e[0m"
 useradd roboshop
-echo ">>>>>> create application directory <<<<<<"
+echo -e  "\e36m>>>>>> create application directory <<<<<<\e[0m"
 mkdir /app 
-echo ">>>>>> download application content <<<<<<"
+echo -e  "\e36m>>>>>> download application content <<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-echo ">>>>>> extract application content <<<<<<"
+echo -e  "\e36m>>>>>> extract application content <<<<<<\e[0m"
 cd /app 
 unzip /tmp/catalogue.zip
 cd /app 
-echo ">>>>>> download nodejs dependencies <<<<<<"
+echo -e  "\e36m>>>>>> download nodejs dependencies <<<<<<\e[0m"
 npm install 
-echo ">>>>>> install mongo client <<<<<<"
+echo -e  "\e36m>>>>>> install mongo client <<<<<<\e[0m"
 yum install mongodb-org-shell -y
-echo ">>>>>> load catalogue schema <<<<<<"
+echo -e  "\e36m>>>>>> load catalogue schema <<<<<<\e[0m"
 mongo --host mongodb.vyshu.online </app/schema/catalogue.js
-echo ">>>>>> start catalogue service <<<<<<"
+echo -e  "\e36m>>>>>> start catalogue service <<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue 
 systemctl restart catalogue
