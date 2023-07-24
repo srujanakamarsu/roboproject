99406a699bd98b3264011913362601ec349c7bd4
srujanakamarsu <srujanamythree@gmail.com>
2023-07-24 22:49:29 +0530

    redirecting output to  temp file

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,39 @@
 echo -e  "\e[36m>>>>>> create catalogue service file <<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> create mongodb repo file <<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> install node js repos <<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> install node js <<<<<<\e[0m"
-yum install nodejs -y
+yum install nodejs -y >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> create application user <<<<<<\e[0m"
-useradd roboshop
+useradd roboshop >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> create application directory <<<<<<\e[0m"
-mkdir /app 
+mkdir /app >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> download application content <<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> extract application content <<<<<<\e[0m"
-cd /app 
-unzip /tmp/catalogue.zip
-cd /app 
+cd /app >/tmp/roboshop.log
+unzip /tmp/catalogue.zip >/tmp/roboshop.log
+cd /app >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> download nodejs dependencies <<<<<<\e[0m"
-npm install 
+npm install >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> install mongo client <<<<<<\e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> load catalogue schema <<<<<<\e[0m"
-mongo --host mongodb.vyshu.online </app/schema/catalogue.js
+mongo --host mongodb.vyshu.online </app/schema/catalogue.js >/tmp/roboshop.log
+
 echo -e  "\e[36m>>>>>> start catalogue service <<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl restart catalogue
+systemctl daemon-reload >/tmp/roboshop.log
+systemctl enable catalogue >/tmp/roboshop.log
+systemctl restart catalogue >/tmp/roboshop.log
