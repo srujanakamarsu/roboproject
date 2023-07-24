47e1a0bddea23e17c20d7cfab721ca1c6ea1f495
srujanakamarsu <srujanamythree@gmail.com>
2023-07-24 18:21:59 +0530

    roboshop-scripts dns update

--- shipping.sh
+++ shipping.sh
@@ -9,7 +9,7 @@ cd /app
 mvn clean package 
 mv target/shipping-1.0.jar shipping.jar 
 yum install mysql -y 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+mysql -h mysql.vyshu.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
 
 systemctl enable shipping 
 systemctl restart shipping
