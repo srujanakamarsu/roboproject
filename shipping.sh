44367c734087c6910ef334460e2469483a559851
srujanakamarsu <srujanamythree@gmail.com>
2023-07-24 19:46:35 +0530

    shipping.sh updated

--- shipping.sh
+++ shipping.sh
@@ -10,6 +10,6 @@ mvn clean package
 mv target/shipping-1.0.jar shipping.jar 
 yum install mysql -y 
 mysql -h mysql.vyshu.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
-
+systemctl daemon-reload
 systemctl enable shipping 
 systemctl restart shipping
